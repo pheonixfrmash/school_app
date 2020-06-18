bc0d2fd6c7fd10579797ddecec9eccdaea60ba26
pheonixfrmash <65906368+pheonixfrmash@users.noreply.github.com>
2020-06-18 11:58:36 +0530

    Update views.py

--- student/views.py
+++ student/views.py
@@ -701,7 +701,7 @@ def view_student(request, student_id_pk):
 
 
 def send_file_student(request):
-    file_path = "C:/Users/ANAND/Downloads/student.xlsx"
+    file_path = "C:/Users/Downloads/student.xlsx" #Enter your path for master files here
     if os.path.exists(file_path):
         with open(file_path, 'rb') as fh:
             response = HttpResponse(fh.read(), content_type="application/vnd.ms-excel")
